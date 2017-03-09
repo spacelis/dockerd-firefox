185039e140a252cff041bbe6ea3787958555146d
Wen Li <spacelis@gmail.com>
2017-03-09 12:47:43 +0000

    Fix certificates installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ dpkg-reconfigure ca-certificates
 install -o firefox -g firefox -m 700 -d /home/firefox/.pki
 install -o firefox -g firefox -m 700 -d /home/firefox/.pki/nssdb
 certutil -d sql:/home/firefox/.pki/nssdb -N --empty-password
-certutil -d sql:/home/firefox/.pki/nssdb -A -t "C,," -n "Mock CA" -i /usr/local/share/ca-certificates/extra/mock_ca.crt
+certutil -d sql:/home/firefox/.pki/nssdb -A -t "C,," -n "Mock CA" -i /usr/local/share/ca-certificates/extra/*.crt
 
 exec su -ls "/bin/bash" -c "$*" firefox
 # exec bash -c "$*"
